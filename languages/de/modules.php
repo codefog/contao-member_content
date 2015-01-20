3d38c8c0540b5527576691783f14f6eca4a2a47c
Kamil Kuzminski <kamil.kuzminski@gmail.com>
2015-01-20 17:35:44 +0100

    Added the German language files (thanks to Didier Federer)

---
+++ languages/de/modules.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * member_content extension for Contao Open Source CMS
+ *
+ * Copyright (C) 2014 Codefog
+ *
+ * @package member_content
+ * @author  Codefog <http://codefog.pl>
+ * @author  Kamil Kuzminski <kamil.kuzminski@codefog.pl>
+ * @author  Didier Federer <d.federer@designpilot.ch>
+ * @license LGPL
+ */
+
+
+/**
+ * Front end modules
+ */
+$GLOBALS['TL_LANG']['FMD']['member_content'] = array('Mitglieder Inhalt', 'Anzeigen des Mitglieder spezifischen Inhaltes auf der Webseite.');
